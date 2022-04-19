bd70aa0fe6cc6973f0a16fd76aa10a1bbf33e5a7
ATTaylor23 <taylorthorntonalexander23@gmail.com>
2022-04-19 01:36:21 -0400

    HotelPage Update - 1 hotelTest Update -1 hotel xml file created

--- src/test/java/pages/HotelPage.java
+++ src/test/java/pages/HotelPage.java
@@ -1,22 +1,33 @@
 package pages;
 
-import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.DataProvider;
 import utilities.Driver;
+import utilities.PropertyReader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
 public class HotelPage {
 
-    WebElement drive;
+    WebElement driver;
 
     public HotelPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(xpath = "//div[@class='picturefill-background shopping-icon-2']")
+    @FindBy(xpath = "//a[@data-analytics-id='home-shop-2']")
     public WebElement shopHotelsBtn;
 
+    @FindBy(id = "qf-0q-destination")
+    public WebElement cityBox;
 
 
 
