cfab059ab5c612328f272ae276cbdf550532bb08
Zukhrahon <zukhra.doctor1@gmail.com>
2022-04-24 13:08:42 -0500

    Zukhra' second task pushing 04/24/22

--- src/test/java/deltaTests/TestBase.java
+++ src/test/java/deltaTests/TestBase.java
@@ -6,45 +6,72 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 import utilities.Driver;
 import utilities.PropertyReader;
 import utilities.SeleniumUtils;
 
+import java.lang.reflect.Method;
 import java.time.Duration;
 
 public class TestBase {
 
-    WebDriver driver;
+    protected WebDriver driver;
     protected static ExtentReports report;
     protected static ExtentSparkReporter htmlReport;
     protected static ExtentTest logger;
 
 
+    @BeforeSuite(alwaysRun = true)
+    public void setupReport(){
+        report = new ExtentReports();
+        String path = System.getProperty("user.dir") + "/target/extentReports/extentReport.html";
+        System.out.println(path);
+        htmlReport = new ExtentSparkReporter(path);
+        report.attachReporter(htmlReport);
+
+        report.setSystemInfo("Name", "Delta Homepage Test Automation results");
+        report.setSystemInfo("Browser", PropertyReader.getProperties("browser"));
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public  void tearDownReport(){
+        report.flush(); // write the output to HTML file
+    }
+
+
     @BeforeMethod(alwaysRun = true)
-    public void setupMethod(){
+    public void setupMethod(Method method) {
 
         driver = Driver.getDriver();
-
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        logger = report.createTest(method.getName());
 
 
 
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDownMethod(){
+    public void tearDownMethod(ITestResult testResult) {
+        if(testResult.getStatus() == ITestResult.SUCCESS){
+            logger.pass("Test PASSED: " + testResult.getName() );
+        }else if(testResult.getStatus() == ITestResult.SKIP){
+            logger.skip("Test SKIPPED: " + testResult.getName() );
+        }else if(testResult.getStatus() == ITestResult.FAILURE){
+            logger.fail("Test FAILED: " + testResult.getName() );
+            logger.fail(testResult.getThrowable());
+            logger.addScreenCaptureFromPath(SeleniumUtils.getScreenshotOnFailure());
+        }
 
-        SeleniumUtils.getScreenshotOnPass();
-        SeleniumUtils.getScreenshotOnFailure();
-
-        Driver.quitDriver();
-
+//        Driver.quitDriver();
     }
 
 
 
+
 }
+
+
